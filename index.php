41e2b5ea06e80afb72d16101c34c94199e867ce5
Mr. Rosa, Genesis <mrgenesis@hotmail.com>
2019-03-06 18:56:20 -0400

    Parte do código foram movidos para outro arquivo para melhorar a legibilidade.

--- index.php
+++ index.php
@@ -20,96 +20,15 @@ get_header(); // carraga o arquivo header.php
                             </div>
                         </div>
                     </div>
-                    <div class="app-header__mobile-menu">
-                        <div>
-                            <button type="button" class="hamburger hamburger--elastic mobile-toggle-nav">
-                                <span class="hamburger-box">
-                                    <span class="hamburger-inner"></span>
-                                </span>
-                            </button>
-                        </div>
-                    </div>
-                    <div class="app-header__menu">
-                        <span>
-                            <button type="button" class="btn-icon btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav">
-                                <span class="btn-icon-wrapper">
-                                    <i class="fa fa-ellipsis-v fa-w-6"></i>
-                                </span>
-                            </button>
-                        </span>
-                    </div>    <div class="scrollbar-sidebar">
-                        <div class="app-sidebar__inner">
-                            <ul class="vertical-nav-menu">
-                                <li class="app-sidebar__heading">Dashboard</li>
-                                <li>
-                                    <a href="javascript:homepage();" class="mm-active">
-                                        <i class="metismenu-icon pe-7s-home"></i>
-                                        Home
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="javascript:transf();" class="mm-active">
-                                        <i class="metismenu-icon pe-7s-cash"></i>
-                                        Realizar Transferência
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>    <div class="app-main__outer">
+                    <?php get_template_part('parts/menu', 'lateral'); ?>
+                    
+                </div>   
+                 <div class="app-main__outer">
+                    
                     <div class="app-main__inner">
-                        <div class="app-page-title">
-                            <div class="page-title-wrapper">
-                                <div class="page-title-heading">
-                                    <div class="page-title-icon">
-                                        <i class="pe-7s-car icon-gradient bg-mean-fruit">
-                                        </i>
-                                    </div>
-                                    <div>Analytics Dashboard
-                                        <div class="page-title-subheading">This is an example dashboard created using build-in elements and components.
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="page-title-actions">
-                                    <button type="button" data-toggle="tooltip" title="Conta corrente" data-placement="bottom" class="btn-shadow mr-3 btn btn-dark">
-                                        <i class="fa fa-star"></i>
-                                    </button>
-                                     
-                                </div>    </div>
-                        </div>            
-                      
-                        <div class="row">
-                            <div class="col-md-6 col-xl-4">
-                                <div class="card mb-3 widget-content">
-                                    <div class="widget-content-outer">
-                                        <div class="widget-content-wrapper">
-                                            <div class="widget-content-left">
-                                                <div class="widget-heading">Saldo</div>
-                                                <div class="widget-subheading">Pode variar ao longo do dia</div>
-                                            </div>
-                                            <div class="widget-content-right">
-                                                <div class="widget-numbers text-success">1896</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-6 col-xl-4">
-                                <div class="card mb-3 widget-content">
-                                    <div class="widget-content-outer">
-                                        <div class="widget-content-wrapper">
-                                            <div class="widget-content-left">
-                                                <div class="widget-heading">Realizar Tranferências</div>
-                                                <div class="widget-subheading">Deve ter saldo em conta</div>
-                                            </div>
-                                            <div class="widget-content-right">
-                                                button
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>                           
-                        </div>
+          
+                      <?php get_template_part('parts/home'); ?>
+                     
                          
                     </div>
 <?php get_footer(); // carraga o arquivo footer.php
